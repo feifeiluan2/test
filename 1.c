e293643ccae9cc24738b693be648bbda25af9ffc
feifeiluan2 <feifeiluan2@163.com>
2014-11-29 18:21:09 +0800

    Create 1.c
    gg

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void) {
+	
+	int a[5][2] = { 1,2,3,4,5,6,7,8,9,0 };
+	int i,j;	
+	for(i=0;i<5;i++) {
+		for(j=0;j<2;j++) {
+			printf("[%d]\t",a[i][j]);
+		}
+		printf("\n");
+	}
+		
+	
+	return 0;
+}
